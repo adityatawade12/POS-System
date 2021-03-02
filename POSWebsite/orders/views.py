7d730a7af1332f66615caf2f573b276c0aeda2aa
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-03-02 22:09:38 +0530

    menu layout done

--- POSWebsite/orders/views.py
+++ POSWebsite/orders/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+import accounts.views
+from firebase_admin import firestore
+# Create your views here.
+
+db=firestore.client()
+
+def menuItems():
+    docs = db.collection(u'dishes').stream()
+    dishes=[]
+    for doc in docs:
+        dishes.append(doc.to_dict())
+    return dishes
