56de32146404143a1f57eec4d14e991aabcb44ca
Aditya <aditya.tawade@somaiya.edu>
2021-03-04 12:55:10 +0530

    cart

--- POSWebsite/static/js/menu.js
+++ POSWebsite/static/js/menu.js
@@ -1,36 +1,27 @@
 
 var cart=[];
 function myfunct(name){
-    // console.log("Clicked!!");
-    // var name=document.getElementsByClassName("card-title-1").innerContent;
+    
     console.log(name);
     $("#list").append("<li><span><i class='fa fa-trash'></i></span> " + name + "</li>")
     cart.push({itemName:name,itemQty:1})
     console.log(cart)
 };
 
-// Check Off Specific Todos By Clicking
+
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-//Click on X to delete Todo
+
 $("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500,function(){
+	$(this).parent().fadeOut(300,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13){
-		//grabbing new todo text from input
-		var todoText = $(this).val();
-		$(this).val("");
-		//create a new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-	}
-});
+
 
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
