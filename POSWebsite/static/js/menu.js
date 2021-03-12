9d4707b14cb98f842407eb795e87a77e47f74310
Aditya <aditya.tawade@somaiya.edu>
2021-03-12 17:18:51 +0530

    menu

--- POSWebsite/static/js/menu.js
+++ POSWebsite/static/js/menu.js
@@ -1,22 +1,51 @@
 
 var cart=[];
+
 function myfunct(name){
-    
+    var x=0;
     console.log(name);
-    $("#list").append("<li><span><i class='fa fa-trash'></i></span> " + name + "</li>")
-    cart.push({itemName:name,itemQty:1})
+    
+    
     console.log(cart)
-};
-
-
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
+    cart.forEach((item, index) => {
+            if(item.itemName==name){
+                item.itemQty+=1
+                x=1;  
+            }
+            
+        
 });
+if(x!=1){
+    cart.push({itemName:name,itemQty:1})
+    // $("#list").append("<li><span><i class='fa fa-trash'></i></span> " + name +" " +"</li>")
+}
+var int=""
+
+cart.forEach((item, index) => {
+    // $("#list").append("<li><span><i class='fa fa-trash'></i></span> " + item.itemName +" "+item.itemQty +"</li>")
+    int+="<li><span><i class='fa fa-trash'></i></span> <span>" + item.itemName +"</span> <span>"+item.itemQty +"</span></li>"
+    
+
+});
+document.getElementById("list").innerHTML=int
+}
+
 
 
 $("ul").on("click", "span", function(event){
+    var t= $(this).parent().children("span")[1].outerText
+       console.log(t)
+       cart.forEach((item, index) => {
+        if(item.itemName==t){
+            delete cart[index]
+        }
+        
+    
+});
 	$(this).parent().fadeOut(300,function(){
+       
 		$(this).remove();
+
 	});
 	event.stopPropagation();
 });
