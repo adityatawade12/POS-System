270a73de0c8d58038f2944d7d193cca49439f8de
Omkar <omksawant18@gmail.com>
2021-03-14 18:32:50 +0530

    abc

--- POSWebsite/orders/views.py
+++ POSWebsite/orders/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 import accounts.views  
 from firebase_admin import firestore
 # Create your views here.
-<<<<<<< HEAD
-=======
 import accounts.views
 from firebase_admin import firestore
 # Create your views here.
->>>>>>> 7d7e207d6e94cbaef6982c3b656939e2ccd6ea16
 
 db=firestore.client()
 
@@ -17,11 +14,6 @@ def menuItems():
     for doc in docs:
         dishes.append(doc.to_dict())
     return dishes
-<<<<<<< HEAD
-    
-    
-=======
 
 def checkout(request):
     return render(request,'checkout.html')
->>>>>>> 7d7e207d6e94cbaef6982c3b656939e2ccd6ea16
--- POSWebsite/stats/views.py
+++ POSWebsite/stats/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render,redirect
 from accounts.views import curuser
 from orders.views import menuItems
-<<<<<<< HEAD
-=======
-
->>>>>>> 7d7e207d6e94cbaef6982c3b656939e2ccd6ea16
 
 # Create your views here.
 def home(request):
@@ -12,7 +8,6 @@ def home(request):
     us=curuser()
     return render(request,'home.html',{'us':us})
 
-<<<<<<< HEAD
 def home1(request):
     return redirect('/home')
 
@@ -22,11 +17,6 @@ def menu(request):
     us=curuser()
     return render(request,'menu.html',{'us':us,'items':items})
 
-=======
-def home1(request): 
-    return redirect('/home')
-
->>>>>>> 7d7e207d6e94cbaef6982c3b656939e2ccd6ea16
 def contact(request):
     us=curuser()
     return render(request,'contact.html',{'us':us})
@@ -35,8 +25,3 @@ def nav(request):
     us=curuser()
     return render(request,'nav.html',{'us':us})
 
-def menu(request):
-    items=menuItems()
-    print(items)
-    us=curuser()
-    return render(request,'menu.html',{'us':us,'items':items})
