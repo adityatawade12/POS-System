e2bf49cc689f9a0c406ada6f5780bb1eabee9f43
Aditya <aditya.tawade@somaiya.edu>
2021-04-08 15:19:35 +0530

    fetched staff data

--- POSAdminConsole/src/js/employee.js
+++ POSAdminConsole/src/js/employee.js
@@ -1,5 +1,40 @@
-// console.log($(".staff").html())
+console.log($("tbody").html())
 var db = firebase.firestore();
+let staff
+
+function getStaffData(){
+    db.collection("staff").onSnapshot((snapshot) => {
+        staff = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // dishes = data;
+        console.log("All data in 'dishes' collection", staff);
+        var x=""
+        staff.forEach((item)=>{
+            x+=`<tr>
+            <td class="text-center"> ${item.name} </td>
+            <td class="text-center"> ${item.role} </td>
+             <td class="text-center"> Acitve </td>
+            
+        </tr>`
+
+        })
+        $("tbody").html(x)
+        // dishes.forEach((doc) =>{
+        //     str = `${doc["Category"]}`;
+        //     if (!category.includes(str)) {
+        //         category.push(str);
+        //     }
+        // });
+
+        
+                // displayDish(dishes, category);
+        
+        
+    });
+}
+getStaffData();
 
 $(".attendanceBtn").on("click",function(){
     $(".salary").css({"display":"none"})
