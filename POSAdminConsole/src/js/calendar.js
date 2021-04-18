b19455b577a26d6a0b5523be07730310dac4dcb6
Aditya <aditya.tawade@somaiya.edu>
2021-04-18 13:05:59 +0530

    emp

--- POSAdminConsole/src/js/calendar.js
+++ POSAdminConsole/src/js/calendar.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+var calendar=null;
+var eventArray=[]
+document.addEventListener('DOMContentLoaded', loadCalendar(eventArray));
+
+function loadCalendar() {
+  var calendarEl = document.getElementById('calendar');
+    calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
       timeZone: 'local',
       locale:'hi',
-      
-            events: [
-        {
-        // id: 'a',
-        title: 'leave',
-        start: "2021-04-10",
-        end:"2021-04-14"
-        }
-        ,
+      events:eventArray
+    //         events: [
+    //     // {
+    //     // // id: 'a',
+    //     // title: 'leave',
+    //     // start: "2021-04-10",
+    //     // end:"2021-04-14"
+    //     // }
+    //     // ,
     //     {
     //         // id: 'a',
     //         display:"background",
@@ -20,16 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
     //         end:"2021-04-07",
     //         // color:"#541aab"
     //  },
-     {
-        // id: 'a',
-        display:"background",
-        start: new Date(2021,01,04),
-        end:new Date(2021,04,04),
-        color:"#f9545a"
- },
+    
 
-    ]
+    // ]
     });
+    
     calendar.render();
     calendar.setOption('locale', 'en');
-  });
+}
--- POSAdminConsole/src/js/employee.js
+++ POSAdminConsole/src/js/employee.js
@@ -1,7 +1,9 @@
-console.log($("tbody").html())
+
 var db = firebase.firestore();
 let staff
 var emp={}
+var persOrCont=0;
+var arrayEvent=[]
 function getStaffData(){
     db.collection("staff").onSnapshot((snapshot) => {
         staff = snapshot.docs.map((doc) => ({
@@ -9,7 +11,7 @@ function getStaffData(){
           ...doc.data(),
         }));
         // dishes = data;
-        console.log("All data in 'dishes' collection", staff);
+        // console.log("All data in 'dishes' collection", staff);
         var x=""
         staff.forEach((item)=>{
             x+=`<tr id=${item.id}>
@@ -60,22 +62,58 @@ $(".dataBtn").on("click",function(){
 })
 
 $(".staffList").on("click","tr",function(){
-    var empId=$(this).attr('id')
+    staffDataRefresh()
+    // var empId=$(this).attr('id')
     
-    staff.forEach(item=>{
-        if(item.id==empId){
-            emp=item
-            $(".staffName").val(item.name)
-            $(".staffNo").val(item.phone)
-            $(".staffAddress").val(item.address)
-            $(".staffRole").val(item.role)
-            $(".staffDateJoined").val(item.joined)
-            $(".staffEmail").val(item.email)
+    // staff.forEach(item=>{
+    //     if(item.id==empId){
+    //         emp=item
+    //         $(".staffName").val(item.name)
+    //         $(".staffNo").val(item.phone)
+    //         $(".staffAddress").val(item.address)
+    //         $(".staffRole").val(item.role)
+    //         $(".staffDateJoined").val(item.joined)
+    //         $(".staffEmail").val(item.email)
+    //         // try {
+    //         //     calendar.eventSources.remove()
+    //         // } catch (error) {
+                
+    //         // }
+    //         calendar.getEvents().forEach(item1=>{
+    //             item1.remove()
+    //         })
+    //         var arr=Object.values(item.attendance)
+    //         console.log(item.attendance)
+    //     //    arr.forEach(it=>{
+    //     //        console.log(it)
+    //     //    })
+    //         arr.forEach(item2=>{
+    //             calendar.addEvent(item2)
+                
+    //         })
+    //         // calendar=loadCalendar(item.attendance)
+    //         // calendar.fullCalendar( 'addEventSource',  )
+    //         // console.log(calendar.getEventSources())
+    //         // calendar.addEventSource( item.attendance )
             
-        }
-    })
-    console.log(emp)
-   
+    //     }
+    // })
+    // // console.log(emp)
+    // refreshPdocs()
+    // refreshCdocs()
+//     var p="";
+//     emp.personalDocs.forEach(item=>{
+//         p+=`<tr id="${item.docLink}"> <td>${item.docName}</td><td class="text-right"><a  href="#myModal" class="viewBtn  btn btn-primary" data-toggle="modal">View</a></td><td class="text-center"><button class="delPBtn btn btn-danger">Delete</button></td></tr>`
+
+//     })
+//    $(".pdocs").html(p)
+//    p="";
+//    emp.contractDocs.forEach(item=>{
+//     p+=`<tr id="${item.docLink}"> <td>${item.docName}</td><td class="text-right"><a  href="#myModal" class="viewBtn  btn btn-primary" data-toggle="modal">View</a></td><td class="text-center"><button  class="delCBtn btn btn-danger" >Delete</button></td></tr>`
+
+//     })
+//     $(".cdocs").html(p)
+
 
 
 
@@ -101,3 +139,366 @@ $(".updateProfile").on("click",function(){
         console.error("Error updating document: ", error);
     });
 })
+
+$("tbody.pdocs").on("click",".viewBtn",function(){
+    emp.personalDocs.forEach(item=>{
+        if(item.docLink==$(this).parent().parent().attr('id')){
+            
+            $(".modal-body.pdfView").html(`<embed src="https://drive.google.com/file/d/${item.docLink}/preview?usp=sharing" width="100%" height="600vh" />`)
+        }
+        
+    })
+    
+})
+
+$("tbody.cdocs").on("click",".viewBtn",function(){
+    emp.contractDocs.forEach(item=>{
+        if(item.docLink==$(this).parent().parent().attr("id")){
+            
+            $(".modal-body.pdfView").html(`<embed src="https://drive.google.com/file/d/${item.docLink}/preview?usp=sharing" width="100%" height="600vh" />`)
+        }
+
+    })
+    
+})
+
+// document.getElementById('fileInput').onchange = function () {
+//     alert('Selected file: ' + this.value);
+//   };
+
+$("tbody.pdocs").on("click",".delPBtn",function(){
+    // $(".newDocSubmitBtn").css({"display":"none"})
+    // $(".deleteBtn").css({"display":"initial"})
+    emp.personalDocs.forEach(item=>{
+        if(item.docLink==$(this).parent().parent().attr("id")){
+            $(".editDocName").val(item.docName)
+            console.log("sad")
+        }
+    })
+   
+})
+
+$("tbody.cdocs").on("click",".delCBtn",function(){
+    // $(".newDocSubmitBtn").css({"display":"none"})
+    // console.log("y")
+    // $(".deleteBtn").css({"display":"initial"})
+    var cId=$(this).parent().parent().attr("id")
+    delDocs(cId)
+    var docRef = db.collection("staff").doc(emp.id);
+    emp.contractDocs.forEach(item=>{
+        if(item.docLink==cId){
+            docRef.update({
+                contractDocs: firebase.firestore.FieldValue.arrayRemove({
+                    "docLink":item.docLink,
+                    "docName":item.docName
+                })
+            });
+        }
+    })
+    // getStaffData();
+    refreshPdocs()
+    refreshCdocs()
+    
+})
+
+$(".uploadDocFile").change(function(e){
+    console.log(e.target.files[0].name)
+    $(".uploadDocName").text(e.target.files[0].name)
+})
+var x=new Date()
+
+function formatDate(dateObject) {
+    var month=(dateObject.getMonth()+1).toString();
+    
+    var dateT=dateObject.getDate().toString();
+    if(month.length==1){
+        month="0"+month
+    }
+    if(dateT.length==1){
+        dateT="0"+dateObject.getDate().toString()
+    }
+    return dateObject.getFullYear().toString()+"-"+month+"-"+dateT
+}
+
+
+var datestr=formatDate(x)
+console.log(datestr)
+var attobj={}
+$(".present").on("click",function () {
+    
+    try {
+        calendar.getEventById(datestr).remove()
+    } catch (error) {
+        
+    }
+    var endDate=new Date(datestr)
+    endDate.setDate(endDate.getDate()+1)
+    attobj={
+        id:datestr,
+        groupId:"present",
+        start: datestr,
+        end:formatDate(endDate),
+        display:"background",
+        
+      }
+    calendar.addEvent(attobj);
+    updateAttendance()
+    
+})
+$(".absent").on("click",function () {
+    try {
+        calendar.getEventById(datestr).remove()
+    } catch (error) {
+        
+    }
+    var endDate=new Date(datestr)
+    endDate.setDate(endDate.getDate()+1)
+    attobj={
+        id:datestr,
+        groupId:"absent",
+        start: datestr,
+        end:formatDate(endDate),
+        display:"background",
+        color:"#541aab"
+      }
+    calendar.addEvent(attobj);
+    updateAttendance()
+})
+// $('#datetimepicker2').datetimepicker();
+$(".aplv").on("click",function () {
+    var c=0
+    var startDate=new Date($(".std").val())
+    var endDate=new Date($(".ndd").val())
+    endDate.setDate(endDate.getDate()+1)
+    console.log(formatDate(startDate))
+    console.log(formatDate(endDate))
+    calendar.getEvents().forEach(item=>{
+        if((startDate>=item.start && startDate<item.end) || (endDate>=item.start && endDate<item.end)){
+            alert("conflicts")
+            c=1
+        }
+        console.log(item.start)
+        console.log(item.end)
+    })
+    if(c!=1){
+        calendar.addEvent({
+            title:"leave",
+            groupId:"leave",
+            start: formatDate(startDate),
+            end:formatDate(endDate),
+            
+            // display:"background",
+            // color:"#541aab"
+          });
+          console.log(calendar.getEvents())
+          
+          updateAttendance()
+      
+    }
+    
+    //   calendar.getEvents().forEach(item=>{
+    //       console.log(item.start)
+    //       console.log(item.end)
+    //   })
+})
+
+function updateAttendance() {
+    calendar.getEvents().forEach(item=>{
+             
+        arrayEvent.push({
+            title:item.title,
+            start:formatDate(item.start),
+            end:item.end==undefined?formatDate(item.start.getDate()+1):formatDate(item.end),
+            groupId:item.groupId,
+            display:item.display,
+            color:item.color
+        })
+        
+
+    })
+    console.log(arrayEvent)
+    var eventObject={}
+    arrayEvent.forEach((item3,i)=>{
+      for( var m in item3 ) {
+          if ( item3[m] == undefined ) {
+              delete item3[m];
+          }
+      } 
+        eventObject[i]=item3
+    })
+    console.log(eventObject)
+    var docRef = db.collection("staff").doc(emp.id);
+              
+    docRef.update({
+      
+     attendance:eventObject
+        
+ })
+}
+
+
+
+$(".paddDoc").on("click",function(){
+    persOrCont=0;
+    $(".deleteBtn").css({"display":"none"})
+    $(".editSubmitBtn").css({"display":"none"})
+    
+    $(".editDocName").val(" ")
+    $(".uploadDocName").text(" ")
+    $(".uploadDocFile").val("")
+    $(".newDocSubmitBtn").css({"display":"initial"})
+})
+
+$(".caddDoc").on("click",function(){
+    persOrCont=1;
+    $(".editDocName").val(" ")
+    $(".uploadDocFile").val("")
+    $(".uploadDocName").text(" ")
+    $(".deleteBtn").css({"display":"none"})
+    $(".editSubmitBtn").css({"display":"none"})
+    $(".newDocSubmitBtn").css({"display":"initial"})
+})
+
+
+$(".editSubmitBtn").on("click",function(){
+    var docRef = db.collection("staff").doc(emp.id);
+    // db.collection("staff").doc(emp.id).update({
+
+    // })
+    console.log($(".uploadDocFile").val())
+})
+
+$(".deleteBtn").on("click",function(){
+        console.log("del")
+    // $.ajax({
+    //     type: "DELETE",
+    //     beforeSend: function(request) {
+    //         request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+            
+    //     },
+    //     // headers:{ 
+    //     //     "Content-Type": "application/json"
+    //     // },
+    //     url: "https://www.googleapis.com/drive/v2/files/"+id,
+    //     // data:JSON.stringify({
+    //     //     "title":id
+    //     // }),
+    //     // xhr: function () {
+    //     //     var myXhr = $.ajaxSettings.xhr();
+    //     //     if (myXhr.upload) {
+    //     //         myXhr.upload.addEventListener('progress', that.progressHandling, false);
+    //     //     }
+    //     //     return myXhr;
+    //     // },
+    //     success: function (data) {
+    //         console.log(data);
+    //     },
+    //     error: function (error) {
+    //         console.log(error);
+    //     },
+    //     async: true,
+    //     // data: formData,
+        
+    //     cache: false,
+    //     // contentType: false,
+    //     processData: false,
+    //     timeout: 60000
+    // });
+})
+
+function delDocs(id) {
+    getToken()
+     $.ajax({
+        type: "DELETE",
+        beforeSend: function(request) {
+            request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
+            
+        },
+        // headers:{ 
+        //     "Content-Type": "application/json"
+        // },
+        url: "https://www.googleapis.com/drive/v2/files/"+id,
+        // data:JSON.stringify({
+        //     "title":id
+        // }),
+        // xhr: function () {
+        //     var myXhr = $.ajaxSettings.xhr();
+        //     if (myXhr.upload) {
+        //         myXhr.upload.addEventListener('progress', that.progressHandling, false);
+        //     }
+        //     return myXhr;
+        // },
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (error) {
+            console.log(error);
+        },
+        async: true,
+        // data: formData,
+        
+        cache: false,
+        // contentType: false,
+        processData: false,
+        timeout: 60000
+    });
+}
+
+function refreshPdocs() {
+    var p="";
+    emp.personalDocs.forEach(item=>{
+        p+=`<tr id="${item.docLink}"> <td>${item.docName}</td><td class="text-right"><a  href="#myModal" class="viewBtn  btn btn-primary" data-toggle="modal">View</a></td><td class="text-center"><button class="delPBtn btn btn-danger">Delete</button></td></tr>`
+
+    })
+   $(".pdocs").html(p)
+}
+
+function refreshCdocs() {
+   var p="";
+    emp.contractDocs.forEach(item=>{
+     p+=`<tr id="${item.docLink}"> <td>${item.docName}</td><td class="text-right"><a  href="#myModal" class="viewBtn  btn btn-primary" data-toggle="modal">View</a></td><td class="text-center"><button  class="delCBtn btn btn-danger" >Delete</button></td></tr>`
+ 
+     })
+     $(".cdocs").html(p)
+}
+
+function staffDataRefresh(){
+    var empId=$(this).attr('id')
+    
+    staff.forEach(item=>{
+        if(item.id==empId){
+            emp=item
+            $(".staffName").val(item.name)
+            $(".staffNo").val(item.phone)
+            $(".staffAddress").val(item.address)
+            $(".staffRole").val(item.role)
+            $(".staffDateJoined").val(item.joined)
+            $(".staffEmail").val(item.email)
+            // try {
+            //     calendar.eventSources.remove()
+            // } catch (error) {
+                
+            // }
+            calendar.getEvents().forEach(item1=>{
+                item1.remove()
+            })
+            var arr=Object.values(item.attendance)
+            console.log(item.attendance)
+        //    arr.forEach(it=>{
+        //        console.log(it)
+        //    })
+            arr.forEach(item2=>{
+                calendar.addEvent(item2)
+                
+            })
+            // calendar=loadCalendar(item.attendance)
+            // calendar.fullCalendar( 'addEventSource',  )
+            // console.log(calendar.getEventSources())
+            // calendar.addEventSource( item.attendance )
+            
+        }
+    })
+    // console.log(emp)
+    refreshPdocs()
+    refreshCdocs()
+}
